e5046916860386d94270e7f38dcb9f10b8185052
tanboonshing <89240495+tanboonshing@users.noreply.github.com>
2021-08-20 14:14:39 +0800

    Create datavisualization_server.R

---
+++ datavisualization_server.R
@@ -0,0 +1,13 @@
+# Load libraries
+library(shiny)
+library(tidyverse)
+
+# Read in data
+adult <- read_csv("adult.csv")
+# Convert column names to lowercase for convenience 
+names(adult) <- tolower(names(adult))
+
+# Define server logic
+shinyServer(function(input, output) {
+  
+})
